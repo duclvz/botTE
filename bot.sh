19f12875889c1595b5bb24867680945a9c26de4e
Duc Lv <duclvzz@gmail.com>
2016-02-09 07:30:28 +0000

    upload

--- bot.sh
+++ bot.sh
@@ -25,7 +25,7 @@ echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources
 apt-get update
 apt-get clean
 apt-get autoclean
-apt-get autoremove
+apt-get autoremove -y
 apt-get install -y xvfb gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps google-chrome-stable
 dpkg --configure -a
 apt-get install -f -y
