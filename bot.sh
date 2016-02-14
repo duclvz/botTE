0779ad41b68c6a4ef6744ef6d4301b61e703db81
Duc Lv <duclvzz@gmail.com>
2016-02-14 07:05:32 +0000

    fix kilall

--- bot.sh
+++ bot.sh
@@ -26,7 +26,7 @@ apt-get update
 apt-get clean
 apt-get autoclean
 apt-get autoremove -y
-apt-get install -y xvfb gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps google-chrome-stable
+apt-get install -y psmisc xvfb gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps google-chrome-stable
 dpkg --configure -a
 apt-get install -f -y
 if [[ `lsb_release -rs` == "12.04" ]]
