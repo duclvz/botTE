a960800ea7b63c20b57f153a0e91b5d3ee3411aa
Duc Lv <duclvzz@gmail.com>
2016-02-16 03:15:49 +0000

    replace chrome by chromium

--- bot.sh
+++ bot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ `ps -e | grep -c bot.sh` -gt 2 ]; then echo "Already running, i'm killing old process, please run it again!"; killall -9 bot.sh && exit 1; fi
+if [ `ps -e | grep -c bot.sh` -gt 2 ]; then echo "Already running, i'm killing old process, please run it again!"; killall -9 bot.sh; killall -9 Xvfb; killall -9 chrome; killall -9 chromium-browser; killall -9 sleep && exit 1; fi
 usage() { echo -e "Usage: $0 [-t <Timer to restart chrome (seconds)>] [-l <Separate traffic exchange links with space delimiter(in quote)>]\nExample: $0 -t 3600 -l http://22hit...\nExample: $0 -t 3600 -l \"http://22hit... http://247webhit... http://...\"" 1>&2; exit 1; }
 [ $# -eq 0 ] && usage
 while getopts ":ht:l:" arg; do
@@ -19,14 +19,12 @@ done
 if [ -z "${timer}" ] || [ -z "${links}" ]; then
     usage
 fi
-echo "Checking update Chrome and related package..."
-wget --no-check-certificate -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+echo "Checking update Chromium and related package..."
 apt-get update
 apt-get clean
 apt-get autoclean
 apt-get autoremove -y
-apt-get install -y psmisc xvfb gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps google-chrome-stable
+apt-get install -y psmisc xvfb gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps chromium-browser
 dpkg --configure -a
 apt-get install -f -y
 if [[ `lsb_release -rs` == "12.04" ]]
@@ -34,32 +32,34 @@ then
     apt-get install -y defoma x-ttcidfont-conf
     (cd /var/lib/defoma/x-ttcidfont-conf.d/dirs/TrueType && mkfontdir > fonts.dir)
 fi
-echo "Downloading chrome user data dir profile..."
-wget --no-check-certificate https://raw.githubusercontent.com/duclvz/botTE/master/chromeBotTE.tar.gz -O /root/chromeBotTE.tar.gz
-echo "Killing old chrome and virtual X display..."
+echo "Downloading chromium user data dir profile..."
+wget --no-check-certificate https://raw.githubusercontent.com/duclvz/botTE/master/chromiumBotTE.tar.gz -O /root/chromiumBotTE.tar.gz
+echo "Killing old chromium and virtual X display..."
 pkill -9 -o chrome
+pkill -9 -o chromium-browser
 killall -9 chrome
+killall -9 chromium-browser
 killall -9 Xvfb
 killall -9 sleep
 while :
 do
-    echo "Recreating/extracting chrome user data dir..."
+    echo "Recreating/extracting chromium user data dir..."
     rm -fr /root/chromeBotTE/
-    tar -xf /root/chromeBotTE.tar.gz -C /root/
+    tar -xf /root/chromiumBotTE.tar.gz -C /root/
     echo "Starting virtual X display..."
     Xvfb :1 -screen 1 1024x768x16 -nolisten tcp & disown
-    echo "Starting chrome TE viewer..."
+    echo "Starting chromium TE viewer..."
     declare -a chromePIDs
     for i in ${!links[@]}
     do
         echo "Open link ${links[$i]}"
-        DISPLAY=:1.1 google-chrome --no-sandbox --new-window --user-data-dir="/root/chromeBotTE" --disable-popup-blocking --incognito ${links[$i]} & disown
+        DISPLAY=:1.1 chromium-browser --no-sandbox --new-window --user-data-dir="/root/chromeBotTE" --disable-popup-blocking --incognito ${links[$i]} & disown
         chromePIDs[$i]=$!
     done
     sleep ${timer}
     for element in ${chromePIDs[@]}
     do
-        echo "Kill chrome PID $element"
+        echo "Kill chromium PID $element"
         kill $element
     done
     echo "Killing virtual X display..."
